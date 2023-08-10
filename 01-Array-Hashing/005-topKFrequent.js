1557c5d8bc63212a4b934f0ac6ee3b857ebcace6
Anthony Bui <anthonyb50@gmail.com>
2023-08-10 13:54:43 +0200

    Progress on topKFrequents using map. No internet. Need resources for Map prototype

--- 01-Array-Hashing/005-topKFrequent.js
+++ 01-Array-Hashing/005-topKFrequent.js
@@ -19,26 +19,38 @@ Output: [1]
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-  let storage = {};
-  let result = [];
-  let sorted = [];
+//   let storage = {};
+//   let result = [];
+//   let sorted = [];
 
-  for (var numb of nums) {
-      if (!storage[numb]) {
-          storage[numb] = 1;
-      } else {
-          storage[numb]++
-      }
-  }
-  for (var key in storage) {
-      sorted.push([key, storage[key]])
-  }
-  sorted.sort((a,b) => {
-      return b[1] - a[1]
-  });
-  for (let i = 0; i < k; i++) {
-      result.push(sorted[i][0])
-  }
-  console.log(sorted)
-  return result
+//   for (var numb of nums) {
+//       if (!storage[numb]) {
+//           storage[numb] = 1;
+//       } else {
+//           storage[numb]++
+//       }
+//   }
+//   for (var key in storage) {
+//       sorted.push([key, storage[key]])
+//   }
+//   sorted.sort((a,b) => {
+//       return b[1] - a[1]
+//   });
+//   for (let i = 0; i < k; i++) {
+//       result.push(sorted[i][0])
+//   }
+//   return result
+
+let storage = new Map();
+for (let num of nums) {
+    if (!storage.get(num)) {
+        storage.set(num, 1);
+    } else {
+        storage.set(num, storage.get(num) + 1)
+    }
+}
+storage.forEach((item, key) => {console.log(item, key)})
+console.log(storage);
 };
+
+console.log(topKFrequent([1,1,1,2,2,3,3], 2))
