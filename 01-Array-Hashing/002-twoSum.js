1f16dce83c8bcf169b3c72f269c79ab6330b680e
Anthony Bui <anthonyb50@gmail.com>
2023-08-10 13:16:53 +0200

    Fixed descriptions per problem

--- 01-Array-Hashing/002-twoSum.js
+++ 01-Array-Hashing/002-twoSum.js
@@ -1,20 +1,22 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 */
 
 /*
 Example 1:
 
-Input: s = "anagram", t = "nagaram"
-Output: true
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 Example 2:
 
-Input: s = "rat", t = "car"
-Output: false
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
 */
 
 /**
--- 01-Array-Hashing/003-validAnagrams.js
+++ 01-Array-Hashing/003-validAnagrams.js
@@ -1,22 +1,20 @@
 /*
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
 */
 
 /*
 Example 1:
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Input: s = "anagram", t = "nagaram"
+Output: true
 Example 2:
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
+Input: s = "rat", t = "car"
+Output: false
 */
 
 /**
