45a5a9e86250e07e76a3f0887deec626e159dd36
Anthony Bui <anthonyb50@gmail.com>
2023-08-10 13:18:38 +0200

    Rearranged incorrect problem statement. Examples are correct

--- 01-Array-Hashing/003-validAnagrams.js
+++ 01-Array-Hashing/003-validAnagrams.js
@@ -1,9 +1,6 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 */
 
 /*
