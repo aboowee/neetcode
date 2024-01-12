efbcc030d4da61391d4ccb2f4865e2bfa31c9321
Anthony Bui <anthonyb50@gmail.com>
2024-01-11 22:53:37 -0500

    Complete longestConsecutiveSequence

--- 01-Array-Hashing/008-longestConsecutiveSequence.js
+++ 01-Array-Hashing/008-longestConsecutiveSequence.js
@@ -21,5 +21,37 @@ Output: 9
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+  //Input is array of numbers
+  //Output is number of consecutive values
+  //Constraint O(n)
 
+  //Tracker for CURRENT indexed value
+  //At current index, check if next value is in array.
+    //If so, increase number
+    //If not, add to tracker
+
+  if (!nums.length) {
+    return 0;
+  }
+
+  let sortedNums = nums.sort((a, b) => (a - b));
+  let currentStreak = 1;
+  let maximum = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (sortedNums[i] === sortedNums[i-1]) {
+      continue;
+    }
+    if (sortedNums[i] === sortedNums[i-1] + 1) {
+      currentStreak++;
+    } else {
+      maximum = Math.max(currentStreak, maximum);
+      currentStreak = 1;
+    }
+  }
+
+  return Math.max(maximum, currentStreak);
 };
+
+console.log(longestConsecutive([100,4,200,1,3,2]));
+console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]));
