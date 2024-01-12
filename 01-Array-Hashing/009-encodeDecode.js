dd969502a995da62fcbca875a4bd0c63a3fdaf49
Anthony Bui <anthonyb50@gmail.com>
2024-01-12 10:54:34 -0500

    Added encodeDecode problem

---
+++ 01-Array-Hashing/009-encodeDecode.js
@@ -0,0 +1,31 @@
+/*
+String Encode and Decode
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+
+Example 1:
+
+Input: ["neet","code","love","you"]
+
+Output:["neet","code","love","you"]
+Example 2:
+
+Input: ["we","say",":","yes"]
+
+Output: ["we","say",":","yes"]
+*/
+
+class Solution {
+  /**
+   * @param {string[]} strs
+   * @returns {string}
+   */
+  encode(strs) {}
+
+  /**
+   * @param {string} str
+   * @returns {string[]}
+   */
+  decode(str) {}
+}
