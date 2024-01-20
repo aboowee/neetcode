5cdd1a64109ab933901c206b12164a0105f0c100
Anthony Bui <anthonyb50@gmail.com>
2024-01-19 19:48:41 -0500

    Partial completion of generateParenthesis

--- 03-Stacks/004-generateParentheses.js
+++ 03-Stacks/004-generateParentheses.js
@@ -16,5 +16,28 @@ Output: ["()"]
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-  //
+  //Would this be recursive?
+  //Input is a number which defines number of opening parenthesis
+  //Output is array of all combinations
+
+  //Place opening parenthesis, iterate to next, or close
+  //Recursively call with another opening
+
+  //Opening and closing parenthesis must equal n
+  let result = [];
+  let combination = "";
+
+  function combinationCreator (openIncr, closeIncr) {
+      if (openIncr === closeIncr === n) {
+        return combination;
+      }
+      if (openIncr < n) {
+        combination += '('
+        combinationCreator(openIncr + 1, closeIncr);
+      }
+    }
+  }
+
 };
+
+console.log(generateParenthesis(2));
