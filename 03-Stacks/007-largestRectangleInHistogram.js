86922fa6d43375a47c4bd53e73a070a6538b6677
Anthony Bui <anthonyb50@gmail.com>
2024-01-21 21:42:04 -0500

    Stack solution following Neetcode

--- 03-Stacks/007-largestRectangleInHistogram.js
+++ 03-Stacks/007-largestRectangleInHistogram.js
@@ -18,23 +18,47 @@ Output: 4
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function(heights) {
-  //input is array of heights
-  //output is largest area
+// var largestRectangleArea = function(heights) {
+//   //input is array of heights
+//   //output is largest area
 
-  //Calc area from 0 to n heights
-  //Redo from i+1 to n heights
+//   //Calc area from 0 to n heights
+//   //Redo from i+1 to n heights
+//   let maxArea = 0;
+//   let maxHeight = 0;
+
+//   for (let i = 0; i < heights.length; i++) {
+//     maxHeight = heights[i];
+//     maxArea = Math.max(maxHeight, maxArea);
+//     for (let j = i + 1; j < heights.length; j++) {
+//       maxHeight = Math.min(heights[j], maxHeight);
+//       maxArea = Math.max(maxHeight*(j-i+1), maxArea);
+//     }
+//   }
+//   return maxArea;
+// };
+
+var largestRectangleArea = function(heights) {
   let maxArea = 0;
-  let maxHeight = 0;
+  let stack = [];
+  let start;
+  let index;
+  let height;
 
   for (let i = 0; i < heights.length; i++) {
-    maxHeight = heights[i];
-    maxArea = Math.max(maxHeight, maxArea);
-    for (let j = i + 1; j < heights.length; j++) {
-      maxHeight = Math.min(heights[j], maxHeight);
-      maxArea = Math.max(maxHeight*(j-i+1), maxArea);
+    start = i;
+    while (stack.length && stack[stack.length - 1][1] > heights[i]) {
+      [index, height] = stack.pop();
+      maxArea = Math.max(maxArea, height * (i - index));
+      start = index;
     }
+    stack.push([start, heights[i]]);
   }
+
+  for (let j = 0; j < stack.length; j++) {
+    maxArea = Math.max(maxArea, stack[j][1] * (heights.length - stack[j][0]))
+  }
+
   return maxArea;
 };
 
