2089ef0444eae014c8c6d3d9fca85643b3677df8
Anthony Bui <anthonyb50@gmail.com>
2024-01-23 16:45:00 -0500

    Complete longestRepeat with sliding window

--- 05-Sliding-Window/003-longestRepeatingChar.js
+++ 05-Sliding-Window/003-longestRepeatingChar.js
@@ -31,42 +31,60 @@ s consists of only uppercase English letters.
  * @param {number} k
  * @return {number}
  */
+
+// THIS IS INCORRECT. DOES NOT HANDLE IF REPLACING BACKWARDS
+// var characterReplacement = function(s, k) {
+//   //Input: string of letters and Number of letters that can be replaced
+//   //Output: longest substring containing same letter
+
+//   //LOGIC
+//     //Initiate current longest string
+//     //Initiate left
+//     //Iterate through string
+//       //Maybe a while loop for each index until hits k times
+//         //If current index matched last, increase longest string
+//         //If not, replace to match last index
+
+//   let longestString = 0;
+//   let currentCounter = 1;
+//   let right = 0;
+//   let replaced = 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     right = i + 1;
+//     while (replaced <= k && right < s.length) {
+//       if (s[right] !== s[i]) {
+//         replaced++;
+//         if (replaced > k) {
+//           break;
+//         }
+//         currentCounter++;
+//       } else {
+//         currentCounter++;
+//       }
+//       right++;
+//     }
+//     longestString = Math.max(longestString, currentCounter);
+//     currentCounter = 1;
+//     replaced = 0;
+//   }
+
+//   return longestString;
+// };
+
 var characterReplacement = function(s, k) {
-  //Input: string of letters and Number of letters that can be replaced
-  //Output: longest substring containing same letter
-
-  //LOGIC
-    //Initiate current longest string
-    //Initiate left
-    //Iterate through string
-      //Maybe a while loop for each index until hits k times
-        //If current index matched last, increase longest string
-        //If not, replace to match last index
-
   let longestString = 0;
-  let currentCounter = 1;
-  let right = 0;
-  let replaced = 0;
+  let leftPointer = 0;
+  let letterCounter = {};
 
   for (let i = 0; i < s.length; i++) {
-    right = i + 1;
-    while (replaced <= k && right < s.length) {
-      if (s[right] !== s[i]) {
-        replaced++;
-        if (replaced > k) {
-          break;
-        }
-        currentCounter++;
-      } else {
-        currentCounter++;
-      }
-      right++;
+    letterCounter[s[i]] ? letterCounter[s[i]]++ : letterCounter[s[i]] = 1;
+    while ((i - leftPointer + 1) - Math.max(...Object.values(letterCounter)) > k) {
+      letterCounter[s[leftPointer]]--;
+      leftPointer++;
     }
-    longestString = Math.max(longestString, currentCounter);
-    currentCounter = 1;
-    replaced = 0;
+    longestString = Math.max(longestString, i - leftPointer + 1);
   }
-
   return longestString;
 };
 
