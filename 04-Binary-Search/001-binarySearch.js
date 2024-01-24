89ef19cb383e134c566fd28129dd72b8f700a81a
Anthony Bui <anthonyb50@gmail.com>
2024-01-24 10:17:57 -0500

    Review of binary search problem

--- 04-Binary-Search/001-binarySearch.js
+++ 04-Binary-Search/001-binarySearch.js
@@ -22,19 +22,52 @@ Explanation: 2 does not exist in nums so return -1
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-  let start = 0;
-  let end = nums.length;
+// var search = function(nums, target) {
+//   let start = 0;
+//   let end = nums.length;
 
-  while (start <= end) {
-      let midpoint = ~~((start+end)/2);
-      if (nums[midpoint] === target) {
-          return midpoint;
-      } else if (nums[midpoint] > target) {
-          end = midpoint - 1
-      } else {
-          start = midpoint + 1
-      }
-  }
-  return -1;
-};
+//   while (start <= end) {
+//       let midpoint = ~~((start+end)/2);
+//       if (nums[midpoint] === target) {
+//           return midpoint;
+//       } else if (nums[midpoint] > target) {
+//           end = midpoint - 1
+//       } else {
+//           start = midpoint + 1
+//       }
+//   }
+//   return -1;
+// };
+
+var search = function(nums, target) {
+    //Input array of ascending numbers, and target number
+    //Output is index of target
+
+    //LOGIC
+        //While start <= end
+        //Search middle
+            //If it is, return
+            //If higher, start is middle + 1
+            //If lower, end is middle - 1
+            //Recalc middle
+
+    let start = 0;
+    let end = nums.length - 1;
+    let mid = Math.floor((end + start)/2);
+
+    while (start <= end) {
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+        mid = Math.floor((end+start)/2);
+    }
+
+    return -1;
+}
+
+console.log(search([-1,0,3,5,9,12], 9));
+console.log(search([1,2,3,4,5], 6));
